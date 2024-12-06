4dfe40aca5a5d34f4871d696976d0f0a2959e88f
sonu79934 <yogasritha8@gmail.com>
2024-12-06 11:16:21 +0530

    Add files via upload

---
+++ luckyseven.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class luckyseven
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String S=sc.next();
+		System.out.println(S.charAt(6));
+
+	}
+}

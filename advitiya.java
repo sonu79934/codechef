3220ee1969080744729ea72d3b66274d54007759
sonu79934 <yogasritha8@gmail.com>
2024-12-06 11:42:36 +0530

    Add files via upload

---
+++ advitiya.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class advitiya
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n>=16&&n<=18)
+		System.out.println("ADVITIYA");
+		else
+        System.out.println(" WAITING FOR ADVITIYA");
+	}
+}

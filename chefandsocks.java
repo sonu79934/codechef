204768f0f421fb3ae0f6dfdab9e74f8c494a1ca1
sonu79934 <yogasritha8@gmail.com>
2024-12-06 12:06:58 +0530

    Add files via upload

---
+++ chefandsocks.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class chefandsocks
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int a=sc.nextInt();
+		int x=sc.nextInt();
+		int y=sc.nextInt();
+		if (a<=x+y)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+
+	}
+}

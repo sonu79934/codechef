02fbb2247fd50c50e48b003f024513004a0f3015
sonu79934 <yogasritha8@gmail.com>
2024-12-06 12:16:27 +0530

    Add files via upload

---
+++ chefandporele.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class chefandporele
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int x=sc.nextInt();
+		if(x>=7)
+		System.out.println("yes");
+		else
+		System.out.println("no");
+
+	}
+}

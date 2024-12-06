601a1492d1d2f852bb8ce905dedc536cd654a907
sonu79934 <yogasritha8@gmail.com>
2024-12-06 11:35:43 +0530

    Add files via upload

---
+++ acplease.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class acplease
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int t=sc.nextInt();
+        if(t>30)
+        System.out.println("YES");
+        else
+        System.out.println("NO");
+	}
+}

21d13239df06343817713666d6892cfdae39a2ce
sonu79934 <yogasritha8@gmail.com>
2024-12-08 15:29:26 +0530

    Add files via upload

---
+++ luckyclover.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class luckyclover
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int d=(n-1)*3+4;
+        System.out.println(d);
+	}
+}

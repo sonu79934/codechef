09f6b37886942cb4f3137dace886ea833922e6df
sonu79934 <yogasritha8@gmail.com>
2024-12-14 13:39:24 +0530

    Add files via upload

---
+++ ioi2024.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class ioi2024
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		if(n<=8)
+		System.out.println("YES");
+		else
+		System.out.println("NO");
+	}
+}

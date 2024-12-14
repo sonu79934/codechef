3a872792ec15d4bd271f241be0115ba23f6417bf
sonu79934 <yogasritha8@gmail.com>
2024-12-14 13:36:17 +0530

    Add files via upload

---
+++ cricketworldcup.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class cricketworldcup
+{
+	public static void main (String[] args) 
+	{
+	Scanner sc=new Scanner (System.in);
+	int X=sc.nextInt();
+	if(X>=12)
+	System.out.println("YES");
+	else
+	System.out.println("NO");
+
+	}
+}
